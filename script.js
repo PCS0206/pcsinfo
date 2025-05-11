2a5849ccb406d1eb5308210dca9034e79028039f
PCS0206 <127528087+PCS0206@users.noreply.github.com>
2025-05-10 22:56:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// You can add JavaScript functionality here if needed. 
+// For example, if you want to add functionality to the search input or any interactions on the page.
+
+// Sample: Basic alert when a search is entered
+document.querySelector('.search input').addEventListener('input', function() {
+  console.log('Searching for: ' + this.value);
+});
